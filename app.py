b378da88a254e6373d651e1017eba607d0eebb80
Lexa Lechuga <raallero@gmail.com>
2023-08-05 22:02:15 -0600

    Updated Interface

--- app.py
+++ app.py
@@ -1,69 +1,113 @@
 # importar clases del archivo models.py
 import os
+import msvcrt
 from models import CatalogoPelicula
 from models import Pelicula
 
 opcion = None
 opciones = (1,2,3,4)
 
-nombre_catalogo = input("\nIngrese nombre del catalogo:")
+def vhs(nombre):
+    os.system('cls')
+    print("")
+    print(f"Catalogo:{nombre}")
+    print(".------------------------.")
+    print("|\\/////////       90 min |")
+    print("| \/  __  ______  __     |")
+    print("|    /  \|\.....|/  \    |")
+    print("|    \__/|/_____|\__/    |")
+    print("| A                      |")
+    print("|    ________________    |")
+    print("|___/_._o________o_._\___|")
+
+def espera():
+    print("\n                             Presione cualquier tecla para continuar")
+    msvcrt.getch()
+
+def continuar():
+    salir = input("                             Desea continuar el Programa?\n\n                             (Responda 'Si' para continuar): ")
+            
+    if (salir.lower() == 'si'):
+        vhs(" ")
+        nombre_catalogo = input("                             Ingrese nombre del catalogo: ")
+        catalogo = CatalogoPelicula(nombre_catalogo)
+        espera()
+    else:
+        opcion = 4
+
+
+vhs(" ")
+nombre_catalogo = input("                             Ingrese nombre del catalogo: ")
 catalogo = CatalogoPelicula(nombre_catalogo)
+espera()
+
+
 
 while opcion != 4:
-    print("\nOpciones:\n")
-    print("1. Agregar Pelicula")
-    print("2. Listar Pelicula")
-    print("3. Eliminar catálogo películas")
-    print("4. Salir")
+    os.system('cls')
+    print("")
+    print(f"Catalogo:{nombre_catalogo}")
+    print(".------------------------.")
+    print("|\\/////////       90 min | Opciones:")
+    print("| \/  __  ______  __     |")
+    print("|    /  \|\.....|/  \    |   1. Agregar Pelicula")
+    print("|    \__/|/_____|\__/    |   2. Listar Pelicula")
+    print("| A                      |   3. Eliminar catálogo películas")
+    print("|    ________________    |   4. Salir")
+    print("|___/_._o________o_._\___|")
 
-    opcion = input("\nEscribe tu opción ( del 1 al 4):")
-    while opcion.isdigit() == False or int(opcion) not in opciones:   
-         print("\nOpciones:\n")
-         print("1. Agregar Pelicula")
-         print("2. Listar Pelicula")
-         print("3. Eliminar catálogo películas")
-         print("4. Salir")
-        
-         opcion = input("\nEscribe tu opción ( del 1 al 4):")
+    opcion = input("                             Escribe tu opción ( del 1 al 4): ")
+    while opcion.isdigit() == False or int(opcion) not in opciones:
+         os.system('cls')
+         print("")
+         print(f"Catalogo:{nombre_catalogo}")
+         print(".------------------------.")
+         print("|\\/////////        90 min | Opciones:")
+         print("| \/  __  ______  __     |")
+         print("|    /  \|\.....|/  \    |   1. Agregar Pelicula")
+         print("|    \__/|/_____|\__/    |   2. Listar Pelicula")
+         print("| A                      |   3. Eliminar catálogo películas")
+         print("|    ________________    |   4. Salir")
+         print("|___/_._o________o_._\___|")
 
-    opcion = int(opcion)        
+         opcion = input("                             Escribe tu opción ( del 1 al 4): ")
+         
+    opcion = int(opcion)         
         
     if opcion == 1:
         try:
-            nombre_pelicula = input("\nIndique el nombre de la pelicula:")
+            vhs(nombre_catalogo)
+            nombre_pelicula = input("                             Indique el nombre de la pelicula: ")
             pelicula = Pelicula(nombre_pelicula)
             catalogo.agregar_pelicula(pelicula)
         except:
-            print("\nError al agregar pelicula\n\n")
-            nombre_catalogo = input("\nIngrese nombre del catalogo:")
-            catalogo = CatalogoPelicula(nombre_catalogo)
+            print("                             Error al agregar pelicula\n\n")
+            continuar()
         else:
-            print(f"\nPelicula '{pelicula.nombre}' ha sido agregada\n\n")
+            print(f"\n                             Pelicula '{pelicula.nombre}' ha sido agregada")
+            espera()
+
     elif opcion == 2:
         try:
-            print("\n\n")
+            vhs(nombre_catalogo)
+            print("")
             catalogo.listar_peliculas()
+            espera()
         except:
-            print("\nError al listar peliculas\n\n")
-            nombre_catalogo = input("Ingrese nombre del catalogo:")
-            catalogo = CatalogoPelicula(nombre_catalogo)
-        
+            print("                             Error al listar peliculas")
+            continuar()
     elif opcion == 3:
         try:
             catalogo.eliminar_peliculas()
         except:
-            print("\nError al eliminar catalogo\n\n")
-            nombre_catalogo = input("Ingrese nombre del catalogo:")
-            catalogo = CatalogoPelicula(nombre_catalogo)
-
+            vhs(nombre_catalogo)
+            print("\n                             Error al eliminar catalogo")
+            continuar()
         else:
-            print("\nArchivo eliminado\n\n")
-            salir = input("Desea continuar el Programa?\n\n(Responda 'Si' para continuar)")
-            if (salir.lower == 'si'):
-                nombre_catalogo = input("\nIngrese nombre del catalogo:")
-                catalogo = CatalogoPelicula(nombre_catalogo)
-            else:
-                opcion = 4
-                           
+
+            vhs(nombre_catalogo)
+            print("                             Archivo eliminado\n\n")
+            continuar()               
 else:
-    print("\nPrograma finalizado")
+    vhs(nombre_catalogo)
+    print("                             Programa finalizado")
